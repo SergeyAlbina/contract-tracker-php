63b0fb06eff3f2d31d6ba689318f0034db08e04e
Sergey Pisarenko <224198343+SergeyAlbina@users.noreply.github.com>
2026-02-23 07:19:36 +0000

    fix: simplify case id route regex for current router parser

--- src/Modules/Cases/routes.php
+++ src/Modules/Cases/routes.php
@@ -5,13 +5,13 @@ use App\Modules\Cases\CasesController;
 
 /** @var \App\Http\Router $router */
 $router->get('/cases', [CasesController::class, 'index']);
-$router->get('/cases/{id:[0-9a-fA-F\-]{36}}', [CasesController::class, 'show']);
+$router->get('/cases/{id:[0-9a-fA-F-]+}', [CasesController::class, 'show']);
 
 $router->post('/cases', [CasesController::class, 'store'], ['roles' => ['admin', 'manager']]);
-$router->patch('/cases/{id:[0-9a-fA-F\-]{36}}', [CasesController::class, 'update'], ['roles' => ['admin', 'manager']]);
-$router->post('/cases/{id:[0-9a-fA-F\-]{36}}', [CasesController::class, 'update'], ['roles' => ['admin', 'manager']]);
+$router->patch('/cases/{id:[0-9a-fA-F-]+}', [CasesController::class, 'update'], ['roles' => ['admin', 'manager']]);
+$router->post('/cases/{id:[0-9a-fA-F-]+}', [CasesController::class, 'update'], ['roles' => ['admin', 'manager']]);
 
-$router->post('/cases/{id:[0-9a-fA-F\-]{36}}/attributes', [CasesController::class, 'saveAttributes'], ['roles' => ['admin', 'manager']]);
-$router->post('/cases/{id:[0-9a-fA-F\-]{36}}/events', [CasesController::class, 'addEvent'], ['roles' => ['admin', 'manager']]);
-$router->post('/cases/{id:[0-9a-fA-F\-]{36}}/files', [CasesController::class, 'uploadFile'], ['roles' => ['admin', 'manager']]);
-$router->post('/cases/{id:[0-9a-fA-F\-]{36}}/assignees', [CasesController::class, 'assign'], ['roles' => ['admin', 'manager']]);
+$router->post('/cases/{id:[0-9a-fA-F-]+}/attributes', [CasesController::class, 'saveAttributes'], ['roles' => ['admin', 'manager']]);
+$router->post('/cases/{id:[0-9a-fA-F-]+}/events', [CasesController::class, 'addEvent'], ['roles' => ['admin', 'manager']]);
+$router->post('/cases/{id:[0-9a-fA-F-]+}/files', [CasesController::class, 'uploadFile'], ['roles' => ['admin', 'manager']]);
+$router->post('/cases/{id:[0-9a-fA-F-]+}/assignees', [CasesController::class, 'assign'], ['roles' => ['admin', 'manager']]);
